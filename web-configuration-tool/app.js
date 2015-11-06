3b56e3b5333f1650b9caab2e6c826a560d02a4be
Mitul Panchal <mpancha@ncsu.edu>
2015-11-06 17:13:43 -0500

    multi tab UI

--- web-configuration-tool/app.js
+++ web-configuration-tool/app.js
@@ -9,17 +9,32 @@ var gen        = require("./generate_webconfig.js");
 // find configuraiton files from the project directory
 
 var config_file = []
-var out_file = []
+var separator = []
+config_file.push('lentil.yml');
+separator.push(":");
+separator.push("=");
 
-config_file.push('lentil_config.yml');
-out_file.push('js/controllers.js');
-
-for(var cfg in config_file)
-{
+// generate schema forms
+for(var cfg in config_file){
      console.log(config_file[cfg]);
-     console.log(out_file[cfg]);
-     gen.generate_config(config_file[cfg], out_file[cfg]);
+     gen.generate_config(config_file[cfg], "js/"+config_file[cfg]+".js", separator[cfg]);
+     gen.write_page(config_file[cfg]+".js");
 }
+
+// write index page
+
+var fs = require('fs');
+var content = fs.readFileSync("tabs.html", 'utf8');
+content += "<div id=\"tabs\">\n <ul>";
+for(var i in config_file){
+  content += "<li><a href=\"#tabs-"+i+"\">" + config_file[i] + "</a></li>\n";
+}
+content += "</ul>\n";
+for(var i in config_file){
+   content += "<div id=\"tabs-"+i+"\">\n<iframe src=\""+config_file[i]+".js.html\" width=\"1000\" height=\"1400\"></iframe>\n</div>\n";
+}
+content += "</div>\n<\body>\n<\html>\n";
+fs.writeFileSync("index.html", content, "utf8");
 
 app.use('/',express.static(__dirname));
 app.use(bodyparser.json());
--- web-configuration-tool/generate_webconfig.js
+++ web-configuration-tool/generate_webconfig.js
@@ -1,61 +1,75 @@
-exports.generate_config = function(config_file, out_file){
+exports.generate_config = function(config_file, out_file, separator){
 
-var dataParser = require('data-file-parser');
-var fs = require('fs');
-dataParser.parse({
-    in: config_file,    //input file 
-    out: config_file+'.json',    //output file 
-    var: 'configs',        //variable name 
-    // g for global  
-    // i for ignore case 
-    // m fot ^ to be start of every line not whole string 
-    regex: /([a-z\-\_]+): ([a-z\_\\:\/.]+)/gim,
-    as:'key|value' //object keys 
-}).then(function(arr){
-    console.log(arr.length+" configs");
-    console.log(arr);
-var content = "var app = angular.module(\"myModule\", [\"schemaForm\"]);\n\
-app.controller(\"FormController\", function($scope, $http, $templateCache) {\n\
-   $scope.schema = {\n\
+   var dataParser = require('data-file-parser');
+   var fs = require('fs');
+   if( separator == "=" ) 
+      var reg = /([a-z\-\_\ ]+)=([a-z\_\ \\:\/.]+)/gim;
+   else
+      var reg = /([a-z\-\_]+): ([a-z\_\\:\/.]+)/gim;
+
+   dataParser.parse({
+     in: config_file,    //input file 
+     out: config_file+'.json',    //output file 
+     var: 'configs',        //variable name 
+     regex: reg,
+     as:'key|value' //object keys 
+   }).then(function(arr){
+
+     console.log(arr.length+" configs");
+
+     var content = "var app = angular.module(\"myModule\", [\"schemaForm\"]);\n\
+     app.controller(\"FormController\", function($scope, $http, $templateCache) {\n\
+     $scope.schema = {\n\
       \"type\": \"object\",\n\
       \"title\": \"Comment\",\n\
       \"properties\": {\n";
-   var i=0;
-   for (i=0; i < arr.length-1; i++){
-      content += "     \"" + arr[i].key + "\": {\n";
-      content += "       \"title\": " + "\"" + arr[i].key +"\",\n";
-      content += "       \"description\": \""+arr[i].key+"\",\n";
-      content += "       \"type\": \"string\",\n";
-      content += "       \"maxLength\": 100,\n";
-      content += "       \"minLength\": 2,\n";
-      content += "       \"validationMessage\": \"Entered value should be greater than 2 and less than 100 characters.\"\n";
-      content += "     },\n";
-   }
-   content += "     \""+arr[i].key +"\": {\n";
-   content += "       \"title\": "+"\"" + arr[i].key +"\",\n";
-   content += "       \"description\": \"" + arr[i].key +"\",\n";
-   content += "       \"type\": \"string\",\n";
-   content += "       \"maxLength\": 100,\n";
-   content += "       \"minLength\": 2,\n";
-   content += "       \"validationMessage\": \"Entered value should be greater than 2 and less than 100 characters.\"\n";
-   content += "     }\n    },\n   \"required\":[\n";
-   for ( i=0; i < arr.length-1; i++){
-      content += "     \""+arr[i].key +"\",\n";
-   }
-   content += "     \""+arr[i].key +"\"]\n   };\n";
-   content += "     $scope.form = [";
-   for (i = 0; i < arr.length; i++) {
-      content += "    {\n    \"key\": \""+ arr[i].key+"\",\n";
-      content += "    \"placeholder\": \"Enter " + arr[i].key +" Value\",\n";
-      content += "    \"htmlClass\": \"content\",\n";
-      content += "    \"labelHtmlClass\": \"ici_label\",\n";
-      content += "    \"fieldHtmlClass\": \"ici_field\"\n   },";
-   }
-   content += fs.readFileSync('remaining.js', 'utf8');
-   console.log(content);
-   fs.writeFileSync(out_file, content, "utf8");
-})
 
+     var i=0;
+     for (i=0; i < arr.length-1; i++){
+       content += "     \"" + arr[i].key + "\": {\n";
+       content += "       \"title\": " + "\"" + arr[i].key +"\",\n";
+       content += "       \"description\": \""+ arr[i].key.replace(/_/g," ")+"\",\n";
+       content += "       \"type\": \"string\",\n";
+       content += "       \"maxLength\": 100,\n";
+       content += "       \"minLength\": 2,\n";
+       content += "       \"validationMessage\": \"Entered value should be greater than 2 and less than 100 characters.\"\n";
+       content += "     },\n";
+     }
+
+     // last key
+     content += "     \""+arr[i].key +"\": {\n";
+     content += "       \"title\": "+"\"" + arr[i].key +"\",\n";
+     content += "       \"description\": \"" + arr[i].key.replace("_"," ") +"\",\n";
+     content += "       \"type\": \"string\",\n";
+     content += "       \"maxLength\": 100,\n";
+     content += "       \"minLength\": 2,\n";
+     content += "       \"validationMessage\": \"Entered value should be greater than 2 and less than 100 characters.\"\n";
+     content += "     }\n    },\n   \"required\":[\n";
+
+     for ( i=0; i < arr.length-1; i++){
+       content += "     \""+arr[i].key +"\",\n";
+     }
+     content += "     \""+arr[i].key +"\"]\n   };\n";
+     content += "     $scope.form = [";
+     for (i = 0; i < arr.length; i++) {
+       content += "    {\n    \"key\": \""+ arr[i].key+"\",\n";
+       content += "    \"placeholder\": \"Enter " + arr[i].key +" Value\",\n";
+       content += "    \"htmlClass\": \"content\",\n";
+       content += "    \"labelHtmlClass\": \"ici_label\",\n";
+       content += "    \"fieldHtmlClass\": \"ici_field\"\n   },";
+    }
+    content += fs.readFileSync('remaining.js', 'utf8');
+    //console.log(content);
+    fs.writeFileSync(out_file, content, "utf8");
+  })
 };
 
-
+exports.write_page = function(out_file){
+   var fs = require('fs');
+   var content = fs.readFileSync('frame.html', 'utf8');
+   var newJs = "js/" + out_file;
+   console.log(newJs)
+   content = content.replace(/js\/controllers\.js/, "js/"+out_file);
+   console.log(content);
+   fs.writeFileSync(out_file+".html", content, "utf8"); 
+}
