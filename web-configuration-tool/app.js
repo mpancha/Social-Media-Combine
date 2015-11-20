b21749ea3737f8ef1405cf2a72dfa5e3aaceb52d
Mitul Panchal <mpancha@ncsu.edu>
2015-11-20 17:40:41 -0500

    clean up

--- web-configuration-tool/app.js
+++ web-configuration-tool/app.js
@@ -10,9 +10,9 @@ var gen        = require("./generate_webconfig.js");
 var config_file = []
 var config_dir = []
 var separator = []
-config_dir.push('../lentil/');
+config_dir.push('/src/lentil/');
 config_file.push('lentil_config.yml');
-config_dir.push('../sfm/');
+config_dir.push('/src/sfm/');
 config_file.push('sfm_config.txt');
 separator.push(":");
 separator.push("=");
@@ -41,61 +41,7 @@ var dynamic = {
    }
    content += "</div>\n<\body>\n<\html>\n";
    fs.writeFileSync("index.html", content, "utf8");
-   },
-   
-  server: function(){
-
-     var app        = express();
-     app.use('/',express.static(__dirname));
-     app.use(bodyparser.json());
-     app.use(bodyparser.urlencoded({ extended: false }));
-
-     app.post('/Submit/:file',function(request,response){
-       var jsonString = JSON.stringify(request.body);
-       var jsonObj    = JSON.parse(jsonString);
-       var i = config_file.indexOf(request.file);
-       var cfg = config_dir[i] + config_file[i];
-       var type = request.file.indexOf('sfm');
-
-       fs.unlinkSync(cfg);
-
-       fs.appendFileSync(cfg, "defaults: &defaults\n\n");
-
-       for (var myKey in jsonObj) {
-          if (type > -1) {
-             fs.appendFileSync(cfg, myKey + "=" + jsonObj[myKey] + "\n");
-          } else {
-             fs.appendFileSync(cfg, "   " + myKey + ": \"" + jsonObj[myKey] + "\"\n\n");
-          }
-       }
-       if(type == -1)
-       {
-          fs.appendFileSync(cfg, "development:\n  <<: *defaults\n\n");
-          fs.appendFileSync(cfg, "test:\n  <<: *defaults\n\n");
-          fs.appendFileSync(cfg, "staging:\n  <<: *defaults\n\n");
-          fs.appendFileSync(cfg, "production:\n  <<: *defaults\n\n");
-       }
-       response.writeHead(200, {
-         'Content-Type': 'text/plain'
-       });
-
-       response.end('Hello, World!');
-    });
-
-   app.get('/configData', function (req, res){
-     //var committed = YAML.load('/src/web-configuration-tool/committed/values.txt');
-     res.sendStatus(200);
-   });
-
-   var server=app.listen(8080,function(){
-      exec('sh commit.sh');
-      console.log("We have started our server on port 8080");
-    });
-  }
+   }
 }
-var args = process.argv.slice(2);
-if (args == "setup") {
-   dynamic.setup()
-}else{
-   dynamic.server();
-}
+
+dynamic.setup()
